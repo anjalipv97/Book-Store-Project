e6be227aadab7f7e621e866bfbb5d708b9b46e13
Anjali <anjalipv97@gmail.com>
2023-11-11 13:09:13 +0530

    Second Commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT=5558;
+export const mongoDBURL="mongodb+srv://anjalipv14:anjalipv14@bookstore.cvoi8dp.mongodb.net/?retryWrites=true&w=majority";
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import BooksTable from '../components/home/BooksTable';
+import BooksCard from '../components/home/BooksCard';
+import bookLogo from '../assets/book-logo.png';
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState('table');
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://localhost:5558/books')
+      .then((response) => {
+        setBooks(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className='p-4 bg-gradient-to-r from-indigo-400 via-purple-300 to-pink-400 ... h-screen w-screen'>
+      <div className='flex justify-center items-center gap-x-4'>
+        <button
+          className='bg-cyan-500 hover:bg-sky-600 px-4 py-1 rounded-lg'
+          onClick={() => setShowType('table')}
+        >
+          Table
+        </button>
+        <button
+          className='bg-cyan-500 hover:bg-sky-600 px-4 py-1 rounded-lg'
+          onClick={() => setShowType('card')}
+        >
+          Card
+        </button>
+      </div>
+      <div className='flex justify-between items-center'>
+      <div className='flex justify-between items-center'>
+  <img src={bookLogo} alt="image" style={{ width: '90px', height: '90px' }} />
+</div>
+
+        <h1 className='text-3xl my-text-2xl font-serif  text-violet-900 leading-tight mb-88'>Books List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-green-950 text-4xl font-bold' />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : showType === 'table' ? (
+        <BooksTable books={books} />
+      ) : (
+        <BooksCard books={books} />
+      )}
+    </div>
+  );
+};
+
+export default Home;
