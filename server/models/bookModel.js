c9830c421fe5a6b3510c71ab450757f8a687f17c
Anjali <anjalipv97@gmail.com>
2023-12-04 22:02:59 +0530

    integrated authentication flows

---
+++ server/models/bookModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Book = mongoose.model('Book', bookSchema);
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
